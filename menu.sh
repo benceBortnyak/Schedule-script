a697ff678e9ef133af314a7491bb79302e6f55f5
benceBortnyak <bbortnyak@gmail.com>
2019-09-07 14:53:44 +0200

    ready

--- menu.sh
+++ menu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 clear
 chmod 755 upComing.awk
-
 while true
 
     do
@@ -16,16 +15,42 @@ while true
         if [ $input == 1 ]
         then
         clear
-        ./upComing.awk bus.txt
-            
+        hour=$(date +%H)
+        minute=$(date +%M)
+        result=$(awk -v h=$hour -v m=$minute -f upComing.awk bus.txt)
+            if [[ ! $result ]]
+            then
+                echo "No buses today"
+            else
+                echo "$result"
+            fi
+    
         elif [ $input == 2 ]
         then
-        ./upComing.awk bus.txt 
-     
+        clear
+        read -p 'Hour: ' inHour
+        read -p 'Minute: ' inMinute
+        
+        result2=$(awk -v h=$inHour -v m=$inMinute -f upComing.awk bus.txt)
+
+            if [[ ! $result2 ]]
+            then
+                echo "No buses today"
+            else
+                echo "$result2"
+            fi
+
         elif [ $input == 3 ]
         then
         clear
-            
+        read -p 'Enter the line: ' lineIn
+        result3=$(awk -v line=$lineIn '{if($1 == line){ print "Line: ",$1 ,$2 ":" $3} }' bus.txt)
+            if [[ ! $result3 ]]
+            then
+                echo "No such line"
+            else
+                echo "$result3"
+            fi
         else
             echo 'No such an option'
         fi
--- upComing.awk
+++ upComing.awk
@@ -1,13 +1,11 @@
 #!/usr/bin/awk -f
 
 BEGIN{
-    var=ARGV[1]
-    timeHour=strftime("%H", systime())
-    timeMinute=strftime("%M", systime())
+    timeHour=h
+    timeMinute=m
     print "Upcoming buses today, after : " timeHour ":" timeMinute
 }
 {
-    print var
     if(($2 + 0)>=(timeHour + 0)){
         if($2+0 == timeHour + 0 && $3 + 0 > timeMinute+ 0 ){
             print "Line: ",$1 " Departure: " $2 ":" $3
